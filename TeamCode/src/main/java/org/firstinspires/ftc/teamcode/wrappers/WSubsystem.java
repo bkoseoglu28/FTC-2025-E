a3544c1fdf6beb0c92513083c8b0969ff6684f9b
Berk "Vually" Köseoğlu <40823382+bkoseoglu28@users.noreply.github.com>
2025-10-24 01:51:49 +0300

    full delete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/WSubsystem.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.wrappers;
-
-public abstract class WSubsystem {
-
-    public abstract void periodic();
-    public abstract void read();
-    public abstract void write();
-    public abstract void reset();
-}
